117f34b9cd32a1b4a64c1135ffa515e9ea282dca
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-03 19:13:37 -0400

    :ballot_box_with_check: length-of-last-word [Runtime: 28 ms; Memory: 16.5 MB]

--- string/length-of-last-word.py
+++ string/length-of-last-word.py
@@ -1,5 +1,5 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = [char for char in s.split(" ") if char.isalnum()]
+        s = s.strip().split(" ")
         return len(s[-1])
         
