5ec06508be4696f30bb367cc775954cf6b0ac32c
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-07 12:03:15 -0400

    :ballot_box_with_check: happy-number [Runtime: 46 ms; Memory: 16.5 MB]

---
+++ hash-table/happy-number.py
@@ -0,0 +1,41 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        visited = set()
+
+        def happy_function(num): 
+            total = 0
+            for c in str(num):
+                total += int(c) ** 2
+            return total
+
+        res = n
+        while res != 1: 
+            res = happy_function(res)
+            if res in visited:
+                return False
+            visited.add(res)
+
+        return True
+
+
+
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         num = str(int(num[0])**2 + int(num[1])**2)
+#         total = 0
+#         i = 0
+#         while len(num) > 1:
+#             total += int(num[i]) ** 2
+#             i += 1
+#             if i == len(num):
+#                 num = str(total)
+#                 i = 0
+
+#         if int(num) == 1:
+#             return True
+#         else:
+#             return False
+        
+
+        
