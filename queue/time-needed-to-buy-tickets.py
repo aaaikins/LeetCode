a4a8a5d1337b0269e6a0382a7c4e7c98d578e609
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-12 23:15:52 -0400

    :ballot_box_with_check: time-needed-to-buy-tickets [Runtime: 54 ms; Memory: 16.5 MB]

---
+++ queue/time-needed-to-buy-tickets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        sec = 0
+        i = 0
+        while tickets[k] > 0:
+            if tickets[i] != 0:
+                tickets[i] -= 1
+                sec += 1
+                i += 1
+            else:
+                i += 1   
+            if i == len(tickets):
+                i = 0
+
+        return sec
+        
