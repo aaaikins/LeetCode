a4ce0a2eb8b52d7bf8a3978a045f74b48257f1a6
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-14 21:26:45 -0400

    :ballot_box_with_check: palindrome-number [Runtime: 41 ms; Memory: 16.6 MB]

--- math/palindrome-number.py
+++ math/palindrome-number.py
@@ -1,4 +1,15 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        return str(x)==str(x)[::-1]
+        if x < 0: 
+            return False
+        if x != 0 and x % 10 == 0: 
+            return False
+
+        original = x
+        reversed = 0
+        while (x != 0):
+            reversed = reversed * 10 + x % 10
+            x //= 10
+        
+        return original == reversed
         
