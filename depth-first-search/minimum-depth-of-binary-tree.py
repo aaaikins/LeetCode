f12df56503000100928540a8e8bf46ce8cd0fa41
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-16 18:44:24 -0400

    :ballot_box_with_check: minimum-depth-of-binary-tree [Runtime: 249 ms; Memory: 43.1 MB]

--- depth-first-search/minimum-depth-of-binary-tree.py
+++ depth-first-search/minimum-depth-of-binary-tree.py
@@ -8,12 +8,12 @@ class Solution:
     def minDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
+        if not root.left and not root.right:
+            return 1
         if not root.left:
             return self.minDepth(root.right) + 1
         if not root.right:
             return self.minDepth(root.left) + 1
 
         return 1 + min(self.minDepth(root.right), self.minDepth(root.left))
-
-        return 1 + min(min_left_dept, min_right_dept)
         
