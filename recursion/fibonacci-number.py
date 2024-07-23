01b2efb385b02e05344939a63507444b671d1b8f
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-07-23 12:56:27 -0400

    :ballot_box_with_check: fibonacci-number [Runtime: 379 ms; Memory: 16.4 MB]

--- recursion/fibonacci-number.py
+++ recursion/fibonacci-number.py
@@ -2,7 +2,8 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
-        if n == 1:
+        elif n == 1:
             return 1
-        return self.fib(n-1) + self.fib(n-2)
+        else:
+            return self.fib(n-1) + self.fib(n-2)
         
