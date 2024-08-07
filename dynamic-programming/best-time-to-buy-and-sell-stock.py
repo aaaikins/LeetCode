2ffd1e0b6a5238c019d7133cf93fd3ff2f7b1f41
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-07 10:34:24 -0400

    :ballot_box_with_check: best-time-to-buy-and-sell-stock [Runtime: 751 ms; Memory: 20.9 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -1,16 +1,18 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        max_profit = 0
-
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maxProfit = 0
         lowest = prices[0]
 
         for price in prices:
+
             if price < lowest:
                 lowest = price
-
-            cur_profit = price - lowest
-
-            max_profit = max(cur_profit, max_profit)
+                
+            profit = price - lowest
+            maxProfit = max(profit, maxProfit)
         
-        return max_profit
-        
+        return maxProfit
