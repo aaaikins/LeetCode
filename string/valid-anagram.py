e525756ca8409c366273ea2865ec73ef24822f18
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-07 11:04:46 -0400

    :ballot_box_with_check: valid-anagram [Runtime: 35 ms; Memory: 13.4 MB]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,17 +1,18 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        mapS = {}
-        mapT = {}
-
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
         if len(s) != len(t):
             return False
+        mapS = {}
+        mapT = {}
 
         for i in range(len(s)):
             mapS[s[i]] = mapS.get(s[i], 0) + 1
             mapT[t[i]] = mapT.get(t[i], 0) + 1
-        
+
         return mapS == mapT
-
-
-
         
