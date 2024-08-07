89b756402aee16b66e0ce410d97a85a9215a616e
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-07 10:10:27 -0400

    :ballot_box_with_check: merge-two-sorted-lists [Runtime: 22 ms; Memory: 11.6 MB]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -1,35 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Create a dummy node to act as the head of the merged list
-        dummy = ListNode()
-        current = dummy
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        mergeList = ListNode()
+        temp = mergeList
         
-        # Iterate through both lists
-        while (list1 and list2):
-
+        while list1 and list2:
             if list1.val < list2.val:
-                current.next = list1
+                temp.next = list1
                 list1 = list1.next
-            else:
-                current.next = list2
+            else: 
+                temp.next = list2
                 list2 = list2.next
-                
-            current = current.next
-        
-        # If one of the lists is not empty, append it to the result
-        if list1:
-            current.next = list1
-        else:
-            current.next = list2
-        
-        # Return the next node of the dummy node, which is the head of the merged list
-        return dummy.next
-        
+            temp= temp.next
 
+        if list1:
+            temp.next = list1
         
-        
+        if list2:
+            temp.next = list2
+
+        return mergeList.next
