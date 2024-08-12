fea9a7832c3897036e73623af631ac654d173fab
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-12 12:34:47 -0400

    :ballot_box_with_check: merge-intervals [Runtime: 133 ms; Memory: 21.4 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        if len(intervals) <= 1:
-            return intervals
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
         merge = [intervals[0]]
-        for start, end in intervals:
+
+        for start, end in intervals[1:]:
             lastEnd = merge[-1][1]
-            if merge and start <= lastEnd:
-                merge[-1][1] = max(end, lastEnd)
+
+            if start <= lastEnd:
+                merge[-1][1] = max(lastEnd, end)
             else:
                 merge.append([start, end])
 
