43a430a86f7b1c3cdd6959999b6267efe20854c9
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-12 13:30:30 -0400

    :ballot_box_with_check: reverse-words-in-a-string [Runtime: 36 ms; Memory: 16.8 MB]

--- two-pointers/reverse-words-in-a-string.py
+++ two-pointers/reverse-words-in-a-string.py
@@ -1,7 +1,7 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        s = s.strip().split(" ")
-        s = [char.strip() for char in s if char.isalnum()]
+        s = s.split(" ")
+        s = [char for char in s if char.isalnum()]
         left = 0
         right = len(s) - 1
         print(s)
