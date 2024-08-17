4a98094913b5fb4770c4655ca7f006ce974781a4
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-17 09:04:53 -0400

    LC-spiral-matrix [Runtime: 38 ms; Memory: 16.5 MB]

--- matrix/spiral-matrix.py
+++ matrix/spiral-matrix.py
@@ -9,12 +9,10 @@ class Solution:
             for i in range(left, right):
                 res.append(matrix[top][i])
             top += 1
-            # print(res)
 
             for i in range(top, bottom):
                 res.append(matrix[i][right-1])
             right -= 1
-            # print(res)
 
             if not (left < right and top < bottom):
                 break
@@ -22,11 +20,11 @@ class Solution:
             for i in range(right-1, left-1, -1):
                 res.append(matrix[bottom -1][i])
             bottom -= 1
-            # print(res)
             
             for i in range(bottom - 1, top -1, -1):
                 res.append(matrix[i][left])
             left += 1
+
 
 
         return res
