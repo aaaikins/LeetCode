83a75c3828275ea4faad2eeab6d64fa907145db3
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-08-28 10:54:55 -0400

    LC-top-k-frequent-elements [Runtime: 89 ms; Memory: 21.2 MB]

--- divide-and-conquer/top-k-frequent-elements.py
+++ divide-and-conquer/top-k-frequent-elements.py
@@ -1,23 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) == k:
-            return nums
-
         hashmap = {}
         for num in nums:
             hashmap[num] = hashmap.get(num, 0) + 1
         
-        map_list = []
-        for key, val in hashmap.items():
-            heappush(map_list, (-val, key))
+        map_list = [(-val, key) for key, val in hashmap.items()]
+        heapify(map_list)
         
-    
         result = []
         while k > 0:
             value, key = heappop(map_list)
-            # print(key)
             result.append(key)
-            print(k)
             k -= 1
 
         return result
