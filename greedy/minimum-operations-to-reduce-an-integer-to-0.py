3e090fef666e5d67961a3a349de7c3b2a9823d99
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-09-02 12:18:23 -0400

    LC-minimum-operations-to-reduce-an-integer-to-0 [Runtime: 34 ms; Memory: 16.4 MB]

--- greedy/minimum-operations-to-reduce-an-integer-to-0.py
+++ greedy/minimum-operations-to-reduce-an-integer-to-0.py
@@ -1,11 +1,12 @@
 class Solution:
     def minOperations(self, n: int) -> int:
+        count = 0
 
-        count = -1
-        while n:
-            n &= n - 1
+        while n > 0:
+            lower = n - 2**(int(log2(n)))
+            upper = 2**(int(log2(n)) + 1) - n
+
+            n = min(lower, upper)
             count += 1
-            
-
-        return count
         
+        return count
