c2a2e83a6e53b10bc5fcf6b082d2fc1ce1eb8933
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-09-13 21:19:04 -0400

    LC-subtree-of-another-tree [Runtime: 61 ms; Memory: 16.8 MB]

--- string-matching/subtree-of-another-tree.py
+++ string-matching/subtree-of-another-tree.py
@@ -4,9 +4,21 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
+
+class Solution:   
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if self.isSameTree(root, subRoot):
+
+        def isSametree(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            
+            return isSametree(p.left, q.left) and isSametree(p.right, q.right)
+
+        if isSametree(root, subRoot):
             return True
         if not root:
             return False
@@ -15,13 +27,4 @@ class Solution:
 
         return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
 
-    def isSameTree(self, p, q):
-        if not p and not q:
-            return True
-        if not p or not q:
-            return False
-        if p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-
-    
+        
