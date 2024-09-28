48f44abfd76023e21cd5b02bf3e6bf1934d869a6
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-09-28 16:46:45 -0400

    LC-sqrtx [Runtime: N/A; Memory: N/A]

--- binary-search/sqrtx.py
+++ binary-search/sqrtx.py
@@ -3,9 +3,9 @@ class Solution:
         if x== 0 or x == 1:
             return x
         nums = [i for i in range(1, x)]
-
+        print(nums)
         l, r = 0, len(nums) - 1
-        base = 0
+        base = 1
         while l != r:
             mid = (l + r)//2
             num = nums[mid]
