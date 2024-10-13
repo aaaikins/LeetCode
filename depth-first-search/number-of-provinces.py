13b48e0af64dbffc35a463696f49bb1f09520b88
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-10-12 23:39:11 -0400

    LC-number-of-provinces [Runtime: 182 ms; Memory: 17.7 MB]

--- depth-first-search/number-of-provinces.py
+++ depth-first-search/number-of-provinces.py
@@ -2,26 +2,17 @@ class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         provinces = 0
         visit = set()
-        rows, cols = len(isConnected), len(isConnected[0])
+        n = len(isConnected)
+        def dfs(start):
+            visit.add(start)
+            for end in range(n):
+                if isConnected[start][end] and end not in visit:
+                    dfs(end)
 
-        def dfs(r, c):
-            if (
-                r not in range(rows)
-                or c not in range(cols)
-                or isConnected[r][c] == 0
-                or (r, c) in visit
-            ):
-                return
+        for start in range(n):
+            if start not in visit:
+                provinces += 1
+                dfs(start)
 
-            visit.add((r, c))
-            directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
-
-        for r in range(rows):
-            for c in range(cols):
-                if isConnected[r][c] == 1 and (r, c) not in visit:
-                    provinces += 1
-                    dfs(r, c)
         return provinces
         
