859e53d6763fddf3db8d5c4201366cc98d371094
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-12-19 22:11:02 -0500

    LC-subdomain-visit-count [Runtime: 7 ms; Memory: 18 MB]

--- string/subdomain-visit-count.py
+++ string/subdomain-visit-count.py
@@ -3,11 +3,8 @@ class Solution:
         hashmap = {}
         for cpdomain in cpdomains:
             count, domain = cpdomain.split(" ")
-            hashmap[domain] = hashmap.get(domain, 0) + int(count)
-            subdomains = domain.split('.')[1:]
-            print(hashmap)
-            # if len(subdomains) == 1:
-            #     hashmap[subdomains] = hashmap.get(subdomains, 0) + int(count)
+            # hashmap[domain] = hashmap.get(domain, 0) + int(count)
+            subdomains = domain.split('.')
             for i in range(len(subdomains)):
                 subdomain = ".".join(subdomains[i:])
                 hashmap[subdomain] = hashmap.get(subdomain, 0) + int(count)
