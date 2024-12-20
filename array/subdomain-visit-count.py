d0ccc2c379484ea7058628bfeac9bb4a5203bb3e
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-12-19 22:09:43 -0500

    LC-subdomain-visit-count [Runtime: 47 ms; Memory: 18 MB]

---
+++ array/subdomain-visit-count.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        hashmap = {}
+        for cpdomain in cpdomains:
+            count, domain = cpdomain.split(" ")
+            hashmap[domain] = hashmap.get(domain, 0) + int(count)
+            subdomains = domain.split('.')[1:]
+            print(hashmap)
+            # if len(subdomains) == 1:
+            #     hashmap[subdomains] = hashmap.get(subdomains, 0) + int(count)
+            for i in range(len(subdomains)):
+                subdomain = ".".join(subdomains[i:])
+                hashmap[subdomain] = hashmap.get(subdomain, 0) + int(count)
+        res = []
+        for k, val in hashmap.items():
+            cpd = str(val) + " " + k
+            res.append(cpd)
+        return res
+        
