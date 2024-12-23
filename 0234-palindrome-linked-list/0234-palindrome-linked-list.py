d387bde7ddea7f75eafb362ead1529a302402f1d
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2024-12-23 10:30:12 -0500

    Time: 40 ms (37.76%), Space: 34.7 MB (54.89%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        
+        def reverseList(head):
+            prev = None
+            cur = head
+
+            while cur:
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+            return prev
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+     
+        rev = reverseList(slow)
+
+        while rev:
+            if head.val != rev.val:
+                return False
+            head = head.next
+            rev = rev.next
+
+        return True
+
+
+        
