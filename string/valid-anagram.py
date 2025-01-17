4ba0330c29dba407a517b596cc90334dee41ce2b
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-01-17 12:52:20 -0500

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        if len(s) != len(t):
-            return False
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         mapS = {}
         mapT = {}
 
         for i in range(len(s)):
             mapS[s[i]] = mapS.get(s[i], 0) + 1
             mapT[t[i]] = mapT.get(t[i], 0) + 1
+        
 
         return mapS == mapT
-        
