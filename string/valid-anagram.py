8d086967d42b129ea780a9d2d1531b0df928a37b
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-01-17 12:53:13 -0500

    LC-valid-anagram [Runtime: N/A; Memory: N/A]

--- string/valid-anagram.py
+++ string/valid-anagram.py
@@ -3,7 +3,7 @@ class Solution:
         mapS = {}
         mapT = {}
 
-        for i in range(len(s)):
+        for i in range(max(len(s), len(t))):
             mapS[s[i]] = mapS.get(s[i], 0) + 1
             mapT[t[i]] = mapT.get(t[i], 0) + 1
         
