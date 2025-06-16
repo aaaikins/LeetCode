659486cc0b8d5dff4645faefa8eca7bdd90320e6
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-06-16 10:14:14 -0400

    LC-3sum [Runtime: N/A; Memory: N/A]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -7,21 +7,23 @@ class Solution:
                 break
             if i > 0 and nums[i-1] == a:
                 continue
-
+            
             l, r = i + 1, len(nums) - 1
             while l < r:
-                threeSum = a + nums[l] + nums[r]
+                total = a + nums[l] + nums[r]
 
-                if threeSum < 0:
+                if total < 0:
                     l += 1
-                elif threeSum > 0:
+                elif total > 0:
                     r -= 1
                 else:
                     res.append([a, nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
-                        l += 1
 
+                    # while l < r and nums[l] == nums[l - 1]:
+                    #     l += 1
+                
         return res
+        
         
