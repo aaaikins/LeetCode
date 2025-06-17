c638763aa8595816102b8ad1ac7646cf3521c123
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-06-17 13:49:10 -0400

    Time: 0 ms (100%), Space: 17.7 MB (80.77%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {')':'(', '}':'{', ']':'['}
+
+        stack = []
+
+        for i in range(len(s)):
+            # print(stack)
+            if stack and s[i] in mapping:
+                top = stack.pop()
+                if mapping[s[i]] != top:
+                    return False
+                # else:
+                #     continue
+            else:
+                stack.append(s[i])
+
+        return len(stack) == 0
+        
+        
