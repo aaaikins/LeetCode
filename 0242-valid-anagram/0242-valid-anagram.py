25b1f40e5c24440a947953c09f303189dc571649
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-06-23 23:09:47 -0400

    Time: 7 ms (89.74%), Space: 17.8 MB (85.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,12 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        mapS = {}
-        mapT = {}
-
-        for i in range(len(s)):
-            mapS[s[i]] = mapS.get(s[i], 0) + 1
-        for i in range(len(t)):
-            mapT[t[i]] = mapT.get(t[i], 0) + 1
+        countS = Counter(s)
+        countT = Counter(t)
         
 
-        return mapS == mapT
+        return countS == countT
