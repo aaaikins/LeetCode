bc0b740fa845f38b4df58350f30502ffb55de56e
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-01 11:48:21 -0400

    LC-find-subsequence-of-length-k-with-the-largest-sum [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/find-subsequence-of-length-k-with-the-largest-sum.py
+++ heap-priority-queue/find-subsequence-of-length-k-with-the-largest-sum.py
@@ -1,12 +1,17 @@
 class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
-        heap = [-num for num in nums]
+        heap = [(-n, i) for i, n in enumerate(nums)]
         heapify(heap)
-        res = [0] * k
+        temp = [0] * len(nums)
 
-        for i in range(k - 1, -1, -1):
-            # print(i)
-            res[i]= - heappop(heap)
+        for _ in range(k):
+            num, idx = heappop(heap)
+            temp[idx] = -num
+        # print(temp)
 
-
+        res = []
+        for i in range(len(nums)):
+            if temp[i] != 0:
+                res.append(temp[i])
+                
         return res
