37acf8a8835353f4dabd25d37d2881d0839825de
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-03 11:43:38 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -1,7 +1,10 @@
+import heapq as hq
+
 class Twitter:
+
     def __init__(self):
         self.tweets = defaultdict(list)
-        self.following = defaultdict(set)
+        self.following = defaultdict(list)
         self.timestamp = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
@@ -10,42 +13,23 @@ class Twitter:
 
     def getNewsFeed(self, userId: int) -> List[int]:
         heap = []
-
-        # Add user's own tweets
         for tweet in self.tweets[userId]:
-            heapq.heappush(heap, tweet)
+            hq.heappush(heap, tweet)
             if len(heap) > 10:
-                heapq.heappop(heap)
-
-        # Add tweets from followees
+                hq.heappop(heap)
+        
         for followee in self.following[userId]:
             for tweet in self.tweets[followee]:
-                heapq.heappush(heap, tweet)
+                hq.heappush(heap, tweet)
                 if len(heap) > 10:
-                    heapq.heappop(heap)
-
-        # Return tweets sorted by most recent
-        return [tweetId for (_, tweetId) in sorted(heap, reverse=True)]
+                    hq.heappop(heap)
+        
+        return [tweet for (_, tweet) in sorted(heap, reverse=True)]
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId != followeeId:
-            self.following[followerId].add(followeeId)
-        print("follow")
-        print(self.following)
-        print(self.tweets)
-
+            self.following[followerId].append(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.following[followerId]:
             self.following[followerId].remove(followeeId)
-        print("unfollow")
-        print(self.following)
-        print(self.tweets)
-
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
