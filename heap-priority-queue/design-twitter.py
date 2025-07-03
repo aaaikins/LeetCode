859b63bf7c6ed50e96584f8fea7a40e9a0f2c697
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-03 11:42:55 -0400

    LC-design-twitter [Runtime: N/A; Memory: N/A]

--- heap-priority-queue/design-twitter.py
+++ heap-priority-queue/design-twitter.py
@@ -1,44 +1,35 @@
+import heapq as hq
+
 class Twitter:
+
     def __init__(self):
         self.tweets = defaultdict(list)
-        self.following = defaultdict(set)
+        self.following = defaultdict(list)
         self.timestamp = 0
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.tweets[userId].append((self.timestamp, tweetId))
-        self.timestamp -= 1
+        self.timestamp += 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
         heap = []
-
-        # Add user's own tweets
         for tweet in self.tweets[userId]:
-            heapq.heappush(heap, tweet)
+            hq.heappush(heap, tweet)
             if len(heap) > 10:
-                heapq.heappop(heap)
-
-        # Add tweets from followees
+                hq.heappop(heap)
+        
         for followee in self.following[userId]:
             for tweet in self.tweets[followee]:
-                heapq.heappush(heap, tweet)
+                hq.heappush(heap, tweet)
                 if len(heap) > 10:
-                    heapq.heappop(heap)
-
-        # Return tweets sorted by most recent
-        return [tweetId for (_, tweetId) in heap]
+                    hq.heappop(heap)
+        
+        return [tweet for (_, tweet) in sorted(heap, reverse=True)]
 
     def follow(self, followerId: int, followeeId: int) -> None:
         if followerId != followeeId:
-            self.following[followerId].add(followeeId)
+            self.following[followerId].append(followeeId)
 
     def unfollow(self, followerId: int, followeeId: int) -> None:
         if followeeId in self.following[followerId]:
             self.following[followerId].remove(followeeId)
-
-
-# Your Twitter object will be instantiated and called as such:
-# obj = Twitter()
-# obj.postTweet(userId,tweetId)
-# param_2 = obj.getNewsFeed(userId)
-# obj.follow(followerId,followeeId)
-# obj.unfollow(followerId,followeeId)
