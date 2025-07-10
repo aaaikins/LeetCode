12ad8d32f90a33421b53fd41bc5b0ad5238e5c78
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-09 22:50:03 -0400

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- sorting/majority-element.py
+++ sorting/majority-element.py
@@ -1,22 +1,32 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         '''
-        [3,2,3]
-             n
-        num = 0 = 3 
-        count = 0 = 1 = 0 = 1
+        [2,2,1,1,1,2,2]
+                     n
+        candidate = None => 2 => 1 => 2
+        count = 0 => 1 => 2 => 1 => 0 => 1 => 0 => 1
+
+        count = 4 - 3 = 1
         '''
 
-        num = 0
+        candidate = None
         count = 0
 
         for n in nums:
             if count == 0:
-                num = n
-            elif n != num:
+                candidate = n
+                count = 1
+            elif n != candidate:
                 count -= 1
             else:
                 count += 1
-
-        return num
+        
+        count = 0
+        for n in nums:
+            if n == candidate:
+                count += 1
+        
+        print(count)
+        if count > n/2:
+            return candidate
         
