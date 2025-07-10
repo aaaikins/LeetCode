405d56a4598ae85a03e08455953f0b5d7d97453a
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-09 22:31:36 -0400

    LC-majority-element-ii [Runtime: 3 ms; Memory: 20.3 MB]

--- sorting/majority-element-ii.py
+++ sorting/majority-element-ii.py
@@ -1,21 +1,36 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        # candidate = 0
-        # count = 0
-        # res = set()
+        candidate1, candidate2 = None, None
+        count1, count2 = 0, 0
+        res = set()
 
-        # for n in nums:
-        #     if count == 0:
-        #         candidate = n
-        #     elif candidate != n:
-        #         count -= 1
-        #     else:
-        #         count += 1
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
+            elif count1 == 0:
+                candidate1 = n
+                count1 = 1
+            elif count2 == 0:
+                candidate2 = n
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        count1 = count2 = 0
+        for n in nums:
+            if n == candidate1:
+                count1 += 1
+            elif n == candidate2:
+                count2 += 1
         
-        # return list(res)
-        n = len(nums)
-        count = Counter(nums)
-        print(count)
-        res = [num for (num, cnt) in count.items() if cnt > (n/3)]
+        result = []
+        n_third = len(nums) // 3
+        if count1 > n_third:
+            result.append(candidate1)
+        if count2 > n_third:
+            result.append(candidate2)
 
-        return res
+        return result
