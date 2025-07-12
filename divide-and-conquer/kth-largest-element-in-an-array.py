087d2da4ba7ad6aeb329514611e78986b785dd72
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-12 18:40:52 -0400

    LC-kth-largest-element-in-an-array [Runtime: 94 ms; Memory: 28.7 MB]

--- divide-and-conquer/kth-largest-element-in-an-array.py
+++ divide-and-conquer/kth-largest-element-in-an-array.py
@@ -1,11 +1,10 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        if nums != []:
-            nums = [-num for num in nums]
-            heapq.heapify(nums)
-        
-        for i in range(k-1):
-            heapq.heappop(nums)
+        heap = [-num for num in nums]
+        heapify(heap)
 
-        return -heapq.heappop(nums)
+        while k > 1:
+            heappop(heap)
+            k -= 1
         
+        return -heappop(heap)
