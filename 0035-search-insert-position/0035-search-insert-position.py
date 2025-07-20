d667c635430cc52f717e281a28a2e3b07eb7a021
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-07-20 16:41:19 -0400

    Time: 0 ms (100%), Space: 18.3 MB (72.41%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -3,14 +3,14 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            mid = (l+r)//2
+            mid = (l+r)// 2
 
             if nums[mid] == target:
                 return mid
-            
-            if nums[mid] < target:
-                l += 1
+            elif nums[mid] < target:
+                l = mid + 1
             else:
-                r -= 1
-        
+                r = mid - 1
+
         return l
+        
