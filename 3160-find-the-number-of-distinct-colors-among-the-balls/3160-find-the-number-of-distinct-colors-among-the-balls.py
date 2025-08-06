36ba3fd7b276f1c7771fcbc630a9812b44f24372
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-08-06 12:54:31 -0400

    Time: 79 ms (48.91%), Space: 68.4 MB (15.28%) - LeetHub

---
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,33 @@
+class Solution:
+    def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
+
+        balltoColor = defaultdict(int)    #O(n) space complexity
+        colorToFreq = defaultdict(int)    #O(n) space complexity 
+        visit = set()
+
+        res = []
+        
+        for ball, color in queries:  #O(n^2) time_complexity 
+            if ball in balltoColor:
+                #get prev color of ball
+                oldColor = balltoColor[ball]
+                
+                #decrement that color because we will change it 
+                colorToFreq[oldColor] -= 1
+                if colorToFreq[oldColor] == 0:
+                    visit.remove(oldColor)
+                #update color of ball
+                balltoColor[ball] = color
+                
+                # increment new color freq by 1
+                colorToFreq[color] += 1
+                visit.add(color)
+                
+            else:
+                balltoColor[ball] = color
+                colorToFreq[color] += 1
+                visit.add(color)
+            
+            res.append(len(visit))
+            
+        return res
