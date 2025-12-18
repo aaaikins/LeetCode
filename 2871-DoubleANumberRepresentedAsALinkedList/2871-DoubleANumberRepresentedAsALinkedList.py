66171348c78bd5a8d99360de377288f4dcc33eef
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:02 -0500

    Create 2871-DoubleANumberRepresentedAsALinkedList

---
+++ 2871-DoubleANumberRepresentedAsALinkedList/2871-DoubleANumberRepresentedAsALinkedList.py
@@ -0,0 +1,36 @@
+# Last updated: 12/18/2025, 1:50:01 PM
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def doubleIt(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(node):
+            prev = None
+            cur = node
+
+            while cur:
+                temp = cur.next
+                cur.next = prev
+                prev = cur
+                cur = temp
+                
+            return prev
+
+        reverse_list = reverse(head)
+        node = reverse_list
+        
+        carry = 0
+        while node:
+            current_double = node.val * 2 + carry
+            node.val = current_double % 10
+            carry = current_double // 10
+            if not node.next and carry:
+                node.next = ListNode(carry)
+                break
+                
+            node = node.next
+        
+        return reverse(reverse_list)
+      
