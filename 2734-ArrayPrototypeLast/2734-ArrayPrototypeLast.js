c208359973a2ac8c0dd3eeb33445cebb0c977c62
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:19 -0500

    Create 2734-ArrayPrototypeLast

---
+++ 2734-ArrayPrototypeLast/2734-ArrayPrototypeLast.js
@@ -0,0 +1,15 @@
+// Last updated: 12/18/2025, 1:50:18 PM
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if (this.length === 0){
+        return -1
+    }
+    return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
