a4ac476544ad4649f3b62f7517a54f13f94b5112
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:09 -0500

    Create 2805-ArrayWrapper

---
+++ 2805-ArrayWrapper/2805-ArrayWrapper.js
@@ -0,0 +1,38 @@
+// Last updated: 12/18/2025, 1:50:09 PM
+/**
+ * @param {number[]} nums
+ * @return {void}
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    let total = 0;
+    if (!this.nums) {
+        return 0
+    }
+    for (num of this.nums){
+        total += num;
+    }
+
+    return total;
+}
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return JSON.stringify(this.nums);
+}
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
