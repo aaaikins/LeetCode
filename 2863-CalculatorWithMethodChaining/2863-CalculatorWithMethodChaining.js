601967ce189877f3972f7aae7b35073592a7882e
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:03 -0500

    Create 2863-CalculatorWithMethodChaining

---
+++ 2863-CalculatorWithMethodChaining/2863-CalculatorWithMethodChaining.js
@@ -0,0 +1,63 @@
+// Last updated: 12/18/2025, 1:50:03 PM
+class Calculator {
+    
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.init = value
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value){
+        this.init += value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value){
+        this.init -= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+        this.init *= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (value === 0) throw new Error("Division by zero is not allowed");
+        this.init /= value
+        return this
+    }
+    
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        this.init **= value
+        return this
+    }
+    
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.init
+    }
+}
