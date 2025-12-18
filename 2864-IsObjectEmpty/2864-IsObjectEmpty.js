7d4b16a622cf4c690fed2314d1227ec3f0ad1aa3
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:03 -0500

    Create 2864-IsObjectEmpty

---
+++ 2864-IsObjectEmpty/2864-IsObjectEmpty.js
@@ -0,0 +1,9 @@
+// Last updated: 12/18/2025, 1:50:02 PM
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    return Object.keys(obj).length === 0;
+    
+};
