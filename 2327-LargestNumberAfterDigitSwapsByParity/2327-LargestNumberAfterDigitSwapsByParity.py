558fca02c95a2f47013cdb12c08baa4f6ac73a7c
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2025-12-18 13:50:27 -0500

    Create 2327-LargestNumberAfterDigitSwapsByParity

---
+++ 2327-LargestNumberAfterDigitSwapsByParity/2327-LargestNumberAfterDigitSwapsByParity.py
@@ -0,0 +1,22 @@
+# Last updated: 12/18/2025, 1:50:27 PM
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        num_str = str(num)
+        even = []
+        odd = []
+
+        for i in num_str:
+            i = int(i)
+            if i %2 == 0:
+                heappush(even, -i)
+            else: 
+                heappush(odd, -i)
+        
+        res = ""
+        for j in num_str:
+            j = int(j)
+            if j %2 == 0:
+                res += str(-heappop(even))
+            else: 
+                res += str(-heappop(odd))
+        return int(res)
