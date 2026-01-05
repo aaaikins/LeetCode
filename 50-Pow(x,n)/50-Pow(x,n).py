2741baa5e21819e907e5f67c17414be4d12804a2
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-01-05 15:25:34 -0500

    File updated at 1/5/2026, 3:25:33 PM

--- 50-Pow(x,n)/50-Pow(x,n).py
+++ 50-Pow(x,n)/50-Pow(x,n).py
@@ -1,25 +1,21 @@
-# Last updated: 1/5/2026, 3:23:29 PM
+# Last updated: 1/5/2026, 3:25:33 PM
 1class Solution:
 2    def myPow(self, x: float, n: int) -> float:
-3        if n == 0:
-4            return 1
-5        
-6        if n < 0:
-7            n = -n
-8            x = 1/x
-9        
-10        results = 1
-11        while n > 0:
-12            if n % 2 == 1:
-13                results *= x
-14            x *= x
-15            n //= 2
-16        
-17        return results 
-18
-19        """
-20        2 ^ 10 = 2^5 * 2^5
-21        2 ^ 5 = 2 * 2^2 *2 ^2
-22        """
-23        
-24        
+3        def halfPow(x, n):
+4            if n == 0:
+5                return 1
+6            
+7            if n < 0:
+8                n = -n
+9                x = 1/x
+10            
+11            half = halfPow(x, n//2)
+12
+13            if n %2 == 0:
+14                return half * half
+15            else:
+16                return x * half * half
+17        
+18        return halfPow(x, n)
+19        
+20        
