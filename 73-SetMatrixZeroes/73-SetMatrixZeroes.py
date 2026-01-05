b2b0f047e48e14388b5755555e698e183ea745d7
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-01-05 15:41:37 -0500

    File updated at 1/5/2026, 3:41:37 PM

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -1,4 +1,4 @@
-# Last updated: 1/4/2026, 7:02:09 PM
+# Last updated: 1/5/2026, 3:41:37 PM
 1class Solution:
 2    def setZeroes(self, matrix: List[List[int]]) -> None:
 3        nRows = len(matrix)
@@ -12,26 +12,30 @@
 11            if matrix[r][0] == 0:
 12                colZero = True
 13
-14        for r in range(nRows):
-15            for c in range(nCols):
-16                if matrix[r][c] == 0:
-17                    matrix[0][c] = 0
-18                    if r > 0:
-19                        matrix[r][0] = 0
-20                    else:
-21                        rowZero = True
-22
-23        for r in range(1, nRows):
-24            for c in range(1, nCols):
-25                if matrix[r][0] == 0 or matrix[0][c] == 0:
-26                    matrix[r][c] = 0
-27
-28        # Fix: only zero first column if needed
-29        if colZero:
-30            for r in range(nRows):
-31                matrix[r][0] = 0
-32
-33        if rowZero:
-34            for c in range(nCols):
-35                matrix[0][c] = 0
+14        for c in range(nCols):
+15            if matrix[0][c] == 0:
+16                rowZero = True
+17
+18        for r in range(nRows):
+19            for c in range(nCols):
+20                if matrix[r][c] == 0:
+21                    matrix[0][c] = 0
+22                    # if r > 0:
+23                    matrix[r][0] = 0
+24                    # else:
+25                    #     rowZero = True
+26
+27        for r in range(1, nRows):
+28            for c in range(1, nCols):
+29                if matrix[r][0] == 0 or matrix[0][c] == 0:
+30                    matrix[r][c] = 0
+31
+32        # Fix: only zero first column if needed
+33        if colZero:
+34            for r in range(nRows):
+35                matrix[r][0] = 0
 36
+37        if rowZero:
+38            for c in range(nCols):
+39                matrix[0][c] = 0
+40
