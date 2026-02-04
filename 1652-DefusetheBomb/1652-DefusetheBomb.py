bb072aae685d4ffd080ea3233ded63810ae30547
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-02-04 18:42:45 -0500

    File updated at 2/4/2026, 6:42:45 PM

--- 1652-DefusetheBomb/1652-DefusetheBomb.py
+++ 1652-DefusetheBomb/1652-DefusetheBomb.py
@@ -1,20 +1,16 @@
-# Last updated: 2/4/2026, 6:40:05 PM
-1class Solution:
-2    def decrypt(self, code: List[int], k: int) -> List[int]:
-3        n = len(code)
-4        res = [0] * n
-5        code = code * n
-6        if k == 0:
-7            return res
-8        
-9        elif k > 0:
-10            for i in range(n):
-11                res[i] = sum(code[i+1: i+ 1 + k])
-12        else:
-13            for i in range(n):
-14                res[i] = sum(code[i+k+n: i+n])
-15        
-16        return res
-17
-18
-19
+# Last updated: 2/4/2026, 6:42:45 PM
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k == 0:
+            return [0] * len(code)
+
+        new_code = code + code + code
+        num = len(code)
+        res = [0] * len(code)
+
+        for i in range(num):
+            if k < 0:
+                res[i] = sum(new_code[num+k+i:i+num])
+            if k > 0:
+                res[i] = sum(new_code[num+i+1:k+num+i+1])
+        return res
