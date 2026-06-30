4a8a70361aa26078c354cb162c18c22ca3922f4e
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-06-30 10:47:22 -0400

    File updated at 6/30/2026, 10:47:21 AM

--- 268-MissingNumber/268-MissingNumber.py
+++ 268-MissingNumber/268-MissingNumber.py
@@ -1,11 +1,8 @@
-# Last updated: 6/30/2026, 10:44:26 AM
+# Last updated: 6/30/2026, 10:47:21 AM
 1class Solution:
 2    def missingNumber(self, nums: List[int]) -> int:
-3        n = len(nums)
-4
-5        numSet = set(nums)
-6
-7        for i in range(n + 1):
-8            if i not in numSet:
-9                return i
-10        
+3        cur_sum = sum(nums)
+4        actual_sum = sum([i for i in range(len(nums)+1)])
+5
+6        return actual_sum - cur_sum
+7        
