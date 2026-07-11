5dd5bacc5841809a74ffe721c69266286afe7d57
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-07-10 22:55:42 -0400

    Create 2240-NumberofWaystoBuyPensandPencils

---
+++ 2240-NumberofWaystoBuyPensandPencils/version/python/2240-NumberofWaystoBuyPensandPencils_20260710_225541.py
@@ -0,0 +1,13 @@
+# Last updated: 7/10/2026, 10:55:41 PM
+1class Solution:
+2    def waysToBuyPensPencils(self, total: int, cost1: int, cost2: int) -> int:
+3        ways = 0
+4        pens = 0
+5
+6        while (pens * cost1) < total + 1:
+7            remaining = total - (pens * cost1)
+8            pencils = (remaining // cost2) + 1
+9            ways += pencils
+10            pens += 1
+11
+12        return ways 
