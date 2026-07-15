ba5c29b8445245d7a85614394d5905030589ea9b
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-07-14 23:00:07 -0400

    File updated at 7/14/2026, 11:00:06 PM

--- 347-TopKFrequentElements/347-TopKFrequentElements.py
+++ 347-TopKFrequentElements/347-TopKFrequentElements.py
@@ -1,21 +1,25 @@
-# Last updated: 7/14/2026, 10:40:06 PM
+# Last updated: 7/14/2026, 11:00:06 PM
 1class Solution:
 2    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-3        # create a hashmap to store freqs
-4        hashmap = {}
-5        for num in nums: # O(N)
-6            hashmap[num] = hashmap.get(num, 0) + 1
-7        
-8        arr = []
-9        for num, freq in hashmap.items(): # O(N)
-10            arr.append((freq, num))
+3        hashmap = {}
+4        for num in nums: # O(N)
+5            hashmap[num] = hashmap.get(num, 0) + 1
+6        
+7        buckets = [[] for i in range(len(nums) + 1)]
+8
+9        for num, freq in hashmap.items(): 
+10            buckets[freq].append(num)
 11        
-12        arr.sort() # O(nlogn)
+12        res = []
 13
-14        res = []
-15        while len(res) < k: # O(k)
-16            freq, num = arr.pop()
-17            res.append(num)
-18        
-19        return res
-20        
+14        for i in range(len(buckets) - 1, -1, -1):
+15            # print(i)
+16            if buckets[i] is None:
+17                continue
+18            for num in buckets[i]:
+19                if len(res) == k:
+20                    return res
+21                res.append(num)
+22        
+23        return res
+24        
