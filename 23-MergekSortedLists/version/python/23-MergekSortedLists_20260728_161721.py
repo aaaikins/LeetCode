881c143dfd741291a531dc78cfec4aa6798f7341
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-07-28 16:17:22 -0400

    Create 23-MergekSortedLists

---
+++ 23-MergekSortedLists/version/python/23-MergekSortedLists_20260728_161721.py
@@ -0,0 +1,33 @@
+# Last updated: 7/28/2026, 4:17:21 PM
+1# Definition for singly-linked list.
+2# class ListNode:
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution:
+7    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+8    
+9        def merge2Lists(l1, l2):
+10            dummy = ListNode()
+11            tail = dummy
+12            while l1 and l2:
+13                if l1.val <= l2.val:
+14                    tail.next = l1
+15                    l1 = l1.next
+16                else: 
+17                    tail.next = l2
+18                    l2 = l2.next
+19                tail = tail.next
+20            
+21            tail.next = l1 or l2
+22            return dummy.next
+23
+24        if len(lists) < 1:
+25            return None
+26
+27        stack = [lists[0]] 
+28        for ll in lists[1:]:
+29            top = stack.pop()
+30            stack.append(merge2Lists(top, ll))
+31        
+32        return stack[-1]
