1bd2e96057f0b982612b26baa383502f0afdfd07
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-07-29 20:31:37 -0400

    File updated at 7/29/2026, 8:31:36 PM

--- 20-ValidParentheses/20-ValidParentheses.py
+++ 20-ValidParentheses/20-ValidParentheses.py
@@ -1,15 +1,42 @@
-# Last updated: 7/29/2026, 7:21:02 PM
+# Last updated: 7/29/2026, 8:31:36 PM
 1class Solution:
 2    def isValid(self, s: str) -> bool:
-3        mapping = {')': '(', '}': '{', ']': '['}
-4        stack = []
-5
-6        for c in s:
-7            if stack and c in mapping:
-8                top = stack.pop()
-9                if mapping[c] != top:
-10                    return False
-11            else:
-12                stack.append(c)
-13        
-14        return not stack
+3        """
+4        mapping = { ")": "(", "}": "{", "]":"["}
+5       
+6       s = "([])"
+7
+8        stack = [(, ]
+9        for c in s:
+10            if stack and c in mapping:
+11                top = stack[-1]
+12                if top == mapping[c]: 
+13                    stack.pop()
+14                else:
+15                    return False
+16            else:
+17                stack.append(c)
+18
+19        return len(stack) == 0
+20
+21        """
+22        
+23        mapping = { ")": "(", "}": "{", "]":"["}
+24
+25        stack = []
+26        for c in s:
+27            if stack and  c in mapping:
+28                top = stack[-1]
+29                if top == mapping[c]:
+30                    stack.pop()
+31                else: 
+32                    return False
+33            
+34            else:
+35                stack.append(c)
+36            
+37        return len(stack) == 0
+38
+39
+40
+41
