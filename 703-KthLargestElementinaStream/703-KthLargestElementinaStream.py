f4568fbee64e61f2310aa4e7833482454c9f715f
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-08-24 17:44:56 -0400

    File updated at 8/24/2026, 5:44:55 PM

--- 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
+++ 703-KthLargestElementinaStream/703-KthLargestElementinaStream.py
@@ -1,24 +1,10 @@
-# Last updated: 8/24/2026, 5:21:39 PM
-1class KthLargest:
-2
-3    def __init__(self, k: int, nums: List[int]):
-4        self.heap = nums
-5        self.k = k
-6        heapify(self.heap)
-7
-8        while len(self.heap) > self.k:
-9            heappop(self.heap)
-10        print(self.heap)
-11
-12    def add(self, val: int) -> int:
-13        heappush(self.heap, val)
-14        if len(self.heap) > self.k:
-15            heappop(self.heap)
-16            
-17        return self.heap[0]
-18        
-19
-20
-21# Your KthLargest object will be instantiated and called as such:
-22# obj = KthLargest(k, nums)
-23# param_1 = obj.add(val)
+# Last updated: 8/24/2026, 5:44:55 PM
+1class Solution:
+2    def findKthLargest(self, nums: List[int], k: int) -> int:
+3        heapify(nums)
+4        
+5        while len(nums) > k:
+6            heappop(nums)
+7            
+8        
+9        return nums[0]
