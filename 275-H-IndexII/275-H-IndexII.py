0b3a2be33a73aa754afd0dde159aab33d33a9368
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-08-25 17:04:19 -0400

    File updated at 8/25/2026, 5:04:19 PM

--- 275-H-IndexII/275-H-IndexII.py
+++ 275-H-IndexII/275-H-IndexII.py
@@ -1,16 +1,17 @@
-# Last updated: 8/25/2026, 5:03:17 PM
+# Last updated: 8/25/2026, 5:04:19 PM
 1class Solution:
 2    def hIndex(self, citations: List[int]) -> int:
 3        n = len(citations)
-4        l, r = 0, n - 1
-5        h = 0
-6
-7        while l <= r:
-8            m = (l + r) // 2
-9            if citations[m] >= n - m:
-10                h = n - m
-11                r = m - 1
-12            else:
-13                l = m + 1
-14
-15        return h
+4
+5        left = 0
+6        right = n - 1
+7
+8        while left <= right:
+9            mid = (left + right) // 2
+10
+11            if citations[mid] >= n - mid:
+12                right = mid - 1
+13            else:
+14                left = mid + 1
+15
+16        return n - left
