f5d6fdffd0d4f18e74ccd377d64e1c7bc91e73c6
Aikins Acheampong <147662162+aaaikins@users.noreply.github.com>
2026-09-20 20:00:39 -0400

    File updated at 9/20/2026, 8:00:39 PM

--- 71-SimplifyPath/71-SimplifyPath.py
+++ 71-SimplifyPath/71-SimplifyPath.py
@@ -1,20 +1,18 @@
-# Last updated: 9/20/2026, 7:56:40 PM
+# Last updated: 9/20/2026, 8:00:39 PM
 1class Solution:
 2    def simplifyPath(self, path: str) -> str:
 3        path = path.split("/")
 4
-5        print(path)
+5        stack = []
 6
-7        stack = []
-8
-9        for dr in path:
-10            if dr == '' or dr == '.':
-11                continue
-12            if dr == "..":
-13                if stack:
-14                    stack.pop()
-15            else:
-16                stack.append(dr)
-17        
-18        return "/" + "/".join(stack)
-19
+7        for part in path:
+8            if part == '' or part == '.':
+9                continue
+10            if part == "..":
+11                if stack:
+12                    stack.pop()
+13            else:
+14                stack.append(part)
+15        
+16        return "/" + "/".join(stack)
+17
